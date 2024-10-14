9c28deb8ded4324eacedf60222ac59209113dede
alvaroloal <alorentealman@gmail.com>
2024-10-14 10:59:10 +0200

    ejercicio colegio

---
+++ APIS REST-SPRINGBOOT/ejcolegio-alvarolorentealman/src/main/java/com/salesianostriana/dam/ejcolegio_alvarolorentealman/EjcolegioAlvaroLorentealmanApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejcolegio_alvarolorentealman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjcolegioAlvaroLorentealmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjcolegioAlvaroLorentealmanApplication.class, args);
+	}
+
+}
---
+++ APIS REST-SPRINGBOOT/ejcolegio-alvarolorentealman/src/test/java/com/salesianostriana/dam/ejcolegio_alvarolorentealman/EjcolegioAlvaroLorentealmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.ejcolegio_alvarolorentealman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjcolegioAlvaroLorentealmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
