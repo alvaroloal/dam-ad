732b0df46af4f485850162024e939b44e923db3b
alvaroloal <alorentealman@gmail.com>
2024-10-15 20:45:07 +0200

    ow

---
+++ APIS REST-SPRINGBOOT/estructura-base/src/main/java/com/salesianostriana/dam/estructura_base/EstructuraBaseApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.estructura_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstructuraBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstructuraBaseApplication.class, args);
+	}
+
+}
---
+++ APIS REST-SPRINGBOOT/estructura-base/src/main/java/com/salesianostriana/dam/estructura_base/modelo/ProductoRepositorio.java
@@ -0,0 +1,8 @@
+package com.salesianostriana.dam.estructura_base.modelo;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
+
+}
