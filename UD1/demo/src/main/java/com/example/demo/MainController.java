221a1afbaec4d4429054838209aec724a659365e
alvaroloal <alorentealman@gmail.com>
2024-12-10 14:30:38 +0100

    primera api

---
+++ UD1/demo/src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@RestController
+public class MainController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "HOLA MUNDO";
+    }
+
+    @GetMapping("/producto/123")
+    public Producto getProducto() {
+        return new Producto(123L, "Un producto molon");
+    }
+    
+
+    record Producto(Long id, String nombre){}
+    
+    
+}
