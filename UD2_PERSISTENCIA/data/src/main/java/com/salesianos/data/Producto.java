7dcb7337b1f09ddb02a18346513a5534a1b47923
alvaroloal <alorentealman@gmail.com>
2025-01-10 13:29:26 +0100

    jpaRepository

--- UD2_PERSISTENCIA/data/src/main/java/com/salesianos/data/Producto.java
+++ UD2_PERSISTENCIA/data/src/main/java/com/salesianos/data/Producto.java
@@ -1,25 +1,45 @@
 package com.salesianos.data;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity//cuando anoto una clase con Entity tengo que generar un id
+@Entity
 @ToString
 @Table(name = "productos")
 public class Producto {
 
-    @Id @GeneratedValue//no se pueden repetir, debe ser un valor inmutable que no cambia nunca //estrategia de generacion del id: AUTO, INDENTITY, SEQUENCE, TABLE
+    @Id @GeneratedValue
     private Long id;
 
+    @Column(length = 4000)
     private String nombre;
+
+    @Column(columnDefinition = "text")
+    private String descripcion;
+
     private double precio;
 
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Producto producto = (Producto) o;
+        return getId() != null && Objects.equals(getId(), producto.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
---
+++ UD2_PERSISTENCIA/data/src/main/java/com/salesianos/data/ProductoRepository.java
@@ -0,0 +1,7 @@
+package com.salesianos.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoRepository
+        extends JpaRepository<Producto, Long> {
+}
