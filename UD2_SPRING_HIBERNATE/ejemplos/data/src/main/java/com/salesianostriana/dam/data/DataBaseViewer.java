f01bcd5b429a8f7d9bfd55817d27647b88d69c03
alvaroloal <alorentealman@gmail.com>
2025-01-16 20:29:52 +0100

    ejemplos

--- UD2_SPRING_HIBERNATE/ejemplos/data/src/main/java/com/salesianostriana/dam/data/DataBaseViewer.java
+++ UD2_SPRING_HIBERNATE/ejemplos/data/src/main/java/com/salesianostriana/dam/data/DataBaseViewer.java
@@ -1,16 +1,13 @@
 package com.salesianostriana.dam.data;
 
-import com.salesianostriana.dam.data.model.Producto;
 import com.salesianostriana.dam.data.repo.ProductoRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 @RequiredArgsConstructor
-public class MainDeMentira{
+public class DataBaseViewer {
 
     private final ProductoRepository repository;
 
