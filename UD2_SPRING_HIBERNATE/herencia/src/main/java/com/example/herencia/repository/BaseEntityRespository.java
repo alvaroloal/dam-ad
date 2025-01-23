91286c81ab8169c19982fe42c1f3ceb9021cdb2b
alvaroloal <alorentealman@gmail.com>
2025-01-23 22:14:24 +0100

    herencia

--- UD2_SPRING_HIBERNATE/herencia/src/main/java/com/example/herencia/repository/BaseEntityRespository.java
+++
@@ -1,7 +0,0 @@
-package com.example.herencia.repository;
-
-import com.example.herencia.model.BaseEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BaseEntityRespository extends JpaRepository <BaseEntity, Long> {
-}
--- UD2_SPRING_HIBERNATE/herencia/src/main/java/com/example/herencia/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.herencia.repository;
-
-import com.example.herencia.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
